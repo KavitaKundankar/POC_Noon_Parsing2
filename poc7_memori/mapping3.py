4d4cc95170e8d46e0925edbc9dab11756cb6468d
kavita <k.kundankar@smartshiphub.com>
2025-11-20 18:22:09 +0530

    poc7

--- poc7_memori/mapping3.py
+++ poc7_memori/mapping3.py
@@ -61,25 +61,15 @@ def rag_mapping(vectordb, extracted_data):
 
     for extracted_key, extracted_value in extracted_data.items():
 
-        # -----------------------------------------
         # 1. CHECK MEMORY FIRST
-        # -----------------------------------------
+
         if extracted_key in memori:
             best_match = memori[extracted_key]
             confidence = 1.0  # From memory → treat as fully trusted
             print(f"[MEMORI] {extracted_key} → {best_match}")
         
         else:
-            # -----------------------------------------
             # 2. VECTOR DB → LLM mapping
-            # -----------------------------------------
-            # docs = vectordb.similarity_search(extracted_key, k=5)
-            # print(docs)
-
-            # candidates = [
-            #     doc.metadata["name"]
-            #     for doc in docs
-            # ]
 
             docs = vectordb.similarity_search_with_score(extracted_key, k=5)
 
@@ -108,17 +98,16 @@ def rag_mapping(vectordb, extracted_data):
 
             print(f"[LLM] {extracted_key} → {best_match} ({confidence})")
 
-            # -----------------------------------------
             # 3. IF CONFIDENCE HIGH → SAVE TO MEMORY
-            # -----------------------------------------
+
             if confidence >= 0.45:
                 memori[extracted_key] = best_match
                 save_memori(memori)
                 print(f"[MEMORI-SAVED] {extracted_key} → {best_match}")
 
-        # -----------------------------------------
+
         # Final mapping dictionary
-        # -----------------------------------------
+
         final_mapping[best_match] = extracted_value
 
         # Write to check file
@@ -128,7 +117,7 @@ def rag_mapping(vectordb, extracted_data):
     return final_mapping
 
 
-MEMORI_FILE = "misuga_memori2.json"
+MEMORI_FILE = "memori/misuga_memori2.json"
 
 def load_memori():
     """Load memory mapping file."""
@@ -142,17 +131,42 @@ def save_memori(mem):
     with open(MEMORI_FILE, "w") as f:
         json.dump(mem, f, indent=2)
 
-# -----------------------------
+
+
 # EXECUTION
-# -----------------------------
+
 vectordb = load_param_db()
 
-with open("misuga_parsed4.json", "r") as f:
+filename = "misuga2"
+with open(f"input/{filename}.json", "r") as f:
     parsed_data = json.load(f)
 
 mapped = rag_mapping(vectordb, parsed_data)
 
-with open("mapped_misuga_parssed4.json", "w") as f:
+with open(f"output/{filename}.json", "w") as f:
     json.dump(mapped, f, indent=2)
 
 print("Mapping completed.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# docs = vectordb.similarity_search(extracted_key, k=5)
+        # print(docs)
+
+        # candidates = [
+        #     doc.metadata["name"]
+        #     for doc in docs
+        # ]
